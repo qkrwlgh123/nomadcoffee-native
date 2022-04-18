bf679bffffa87fc2c700b1f3c1a0a99adec1bc9c
qkrwlgh123 <jiho1024@naver.com>
2022-04-18 17:57:17 +0900

    preload font,image

---
+++ App.js
@@ -0,0 +1,43 @@
+import AppLoading from 'expo-app-loading';
+import { StatusBar } from 'expo-status-bar';
+import React, { useState } from 'react';
+import { Ionicons } from '@expo/vector-icons';
+import * as Font from 'expo-font';
+import { Asset } from 'expo-asset';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  const [loading, setLoading] = useState(true);
+  const onFinish = () => setLoading(false);
+  const preload = () => {
+    const fontsToLoad = [Ionicons.font];
+    const fontPromises = fontsToLoad.map((font) => Font.loadAsync(font));
+    const imagesToLoad = [require('./assets/coffee-img.png')];
+    const imagePromises = imagesToLoad.map((image) => Asset.loadAsync(image));
+    return Promise.all([...fontPromises, ...imagePromises]);
+  };
+  if (loading) {
+    return (
+      <AppLoading
+        startAsync={preload}
+        onError={console.warn}
+        onFinish={onFinish}
+      />
+    );
+  }
+  return (
+    <View style={styles.container}>
+      <Text>따 오 망</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
